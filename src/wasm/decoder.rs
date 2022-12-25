98c57a8d930b7cbf4dfdc6f45ce94d75a96421e2
NewGyu <newgyu+git@kinoboku.net>
2022-12-25 07:38:14 +0000

    ADD: i32, u32

--- src/wasm/decoder.rs
+++ src/wasm/decoder.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use std::io::{BufRead, Read};
+use std::io::{BufRead, Cursor, Read};
 
 ///
 pub trait Decoder
@@ -32,36 +32,60 @@ pub trait WasmModuleBinaryRead: Read + BufRead {
         Ok(u32::from_le_bytes(b.as_slice().try_into()?))
     }
 
-    fn read_u64_leb(&mut self) -> Result<u64> {
+    fn read_u64(&mut self) -> Result<u64> {
         Ok(leb128::read::unsigned(self)?)
     }
+
+    fn read_u32(&mut self) -> Result<u32> {
+        self.read_u64().map(|x| x as u32)
+    }
+
+    fn read_i64(&mut self) -> Result<i64> {
+        Ok(leb128::read::signed(self)?)
+    }
+
+    fn read_i32(&mut self) -> Result<i32> {
+        self.read_i64().map(|x| x as i32)
+    }
+}
+
+pub trait ReadableBytes {
+    fn to_wasm_read(&self) -> Box<dyn WasmModuleBinaryRead + '_>;
 }
 
 impl<R: Read + BufRead> WasmModuleBinaryRead for R {}
 
+impl ReadableBytes for Vec<u8> {
+    fn to_wasm_read(&self) -> Box<dyn WasmModuleBinaryRead + '_> {
+        Box::new(Cursor::new(self))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::io::Cursor;
+
+    use crate::decoder::ReadableBytes;
 
     use super::WasmModuleBinaryRead;
 
     #[test]
     fn test_read() {
         //Given
-        let test_bytes = b"\0asm";
-        let mut cur = Cursor::new(test_bytes);
+        let test_bytes = b"\0asm".to_vec();
+        let mut read = test_bytes.to_wasm_read();
         //When Then
-        let b = cur.read_byte().unwrap();
+        let b = read.read_byte().unwrap();
         assert_eq!(b, 0x00);
 
         //When Then
-        let b = cur.read_bytes(2).unwrap();
+        let b = read.read_bytes(2).unwrap();
         assert_eq!(b, vec![0x61, 0x73]);
-        assert!(cur.has_next().unwrap());
+        assert!(read.has_next().unwrap());
 
-        let b = cur.read_byte().unwrap();
+        let b = read.read_byte().unwrap();
         assert_eq!(b, 0x6d);
-        assert!(!cur.has_next().unwrap());
+        assert!(!read.has_next().unwrap());
     }
 
     #[test]
@@ -81,13 +105,40 @@ mod tests {
     // HEX     =>    0x15     0x86     0xA1
     // LE      =>    0xA1     0x86     0x15
     #[test]
-    fn test_u64_leb() {
+    fn test_unsigned_leb() {
         //Given
         let test_bytes = vec![0xA1_u8, 0x86, 0x15];
-        let mut cur = Cursor::new(test_bytes);
+        let mut cur = Cursor::new(&test_bytes);
         //When
-        let x = cur.read_u64_leb().unwrap();
+        let x = cur.read_u64().unwrap();
         //Then
         assert_eq!(x, 344865);
+
+        //When
+        let mut cur = Cursor::new(&test_bytes);
+        let x = cur.read_u32().unwrap();
+        //Then
+        assert_eq!(x, 344865);
+    }
+
+    #[test]
+    fn test_signed_leb() {
+        //Given
+        let mut buf = [0u8; 64];
+        {
+            let mut buf = &mut buf[..];
+            let _ = leb128::write::signed(&mut buf, -512);
+        }
+        //When
+        let mut cur = Cursor::new(&buf);
+        let x = cur.read_i64().unwrap();
+        //Then
+        assert_eq!(x, -512);
+
+        //When
+        let mut cur = Cursor::new(&buf);
+        let x = cur.read_i32().unwrap();
+        //Then
+        assert_eq!(x, -512);
     }
 }
